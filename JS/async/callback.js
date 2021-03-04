4b60cb19019103ffa1ea2473fb4be0eba6ecddce
GaraFaLand <gitmiu@gmail.com>
2021-03-04 15:03:50 +0900

    callstack test

--- JS/async/callback.js
+++ JS/async/callback.js
@@ -2,18 +2,39 @@
 'use strict';
 
 //Synchronous Callback
-function printImmediately(print) {
-    print();
+// function printImmediately(print) {
+//     print();
+// }
+// console.log('1'); //1
+// printImmediately(() => console.log('hello')); //2
+// setTimeout(() => console.log(2)); //4
+// console.log('3'); //3
+//
+//
+// //Asynchronous Callback
+// function printWithDelay(print, timeout) {
+//     setTimeout(print, timeout);
+// }
+
+// printWithDelay(()=> console.log('async'),2000);
+console.clear()
+
+
+
+function fun1() {
+    console.log('1');
+    fun2();
 }
-console.log('1'); //1
-printImmediately(() => console.log('hello')); //2
-setTimeout(() => console.log(2)); //4
-console.log('3'); //3
 
-
-//Asynchronous Callback
-function printWithDelay(print, timeout) {
-    setTimeout(print, timeout);
+function fun2() {
+    setTimeout(function() {
+        console.log('2');
+    },0);
+    fun3();
 }
 
-printWithDelay(()=> console.log('async'),2000);
+function fun3() {
+    console.log('3');
+}
+
+fun1();
